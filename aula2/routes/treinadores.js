abf9ffb105915725f57396037c965fe2206fa1e7
AnaGFranco <anagsfranco@hotmail.com>
2019-11-28 19:17:15 -0300

    atualizar pokemon do treinador

--- aula2/routes/treinadores.js
+++ aula2/routes/treinadores.js
@@ -11,5 +11,7 @@ router.patch('/:id', controller.update)
 router.delete('/:id', controller.remove)
 router.post('/:treinadorId/pokemons', controller.addPokemon)
 router.patch('/:treinadorId/pokemons/:pokemonId/treinar', controller.treinarPokemon)
+router.get('/:treinadorId/pokemons/:pokemonId/', controller.getPokemonByTreinador)
+router.get('/:treinadorId/pokemons', controller.getPokemons)
 
 module.exports = router
